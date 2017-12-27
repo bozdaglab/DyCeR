5df2c1319cae052eb708cc5a5b92919bc274ff4f
Duc Do Local <ducdh1210@gmail.com>
2017-12-27 11:13:25 -0600

    update

--- ceRNA_helper/create_pair.R
+++
@@ -1,9 +0,0 @@
-### ----- variables  ------#####
-lasso.list = lasso.result$final.lasso.list
-
-### ----- analysis  ------#####
-pair.dt = as.data.table(t(combn(names(lasso.list),2)))
-colnames(pair.dt) = c("RNAi", "RNAj"); setkeyv(pair.dt, c("RNAi","RNAj"))
-
-### ----- cleanup  ------#####
-rm(lasso.list)
